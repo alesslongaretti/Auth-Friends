102ff2e1aadfc1e0bf42700a08aa2d978b716f3b
alesslongaretti <alesslongaretti@gmail.com>
2020-02-18 19:26:42 -0500

    completed step 4 and 5

---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+
+class FriendsList extends React.Component {
+    render(){
+        return (
+            <div></div>
+        )
+    }
+}
