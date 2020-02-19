17dfbe40fcbd23e774f0ec9a1e3120fbc218e51e
alesslongaretti <alesslongaretti@gmail.com>
2020-02-18 19:58:47 -0500

    completed step 6

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,23 +1,41 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-import Login from "./components/Login"
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import Login from "./components/Login";
+import FriendsList from "./components/FriendsList";
+import PrivateRoute from "./components/PrivateRoute";
+import "./App.css";
 
-function App() {
-  return (
-    <Router>
-    <div className="App">
-      <ul>
-        <li>
-        </li>
-      </ul>
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Route component={Login}/>
-      </Switch>
-    </div>
-    </Router>
-  );
+function App() {
+  return (
+    <Router>
+          
+      <div className="App">
+              
+        <ul>
+                  
+          <li>
+            <Link to="/login">Login</Link>
+                    
+          </li>
+          <li>
+            <Link to="/protected">Protected</Link>
+          </li>
+                
+        </ul>
+              
+        <Switch>
+          <PrivateRoute exact path="/protected" component={FriendsList} />
+                  
+          <Route path="/login" component={Login} />
+                  
+          <Route component={Login} />
+                
+        </Switch>
+            
+      </div>
+          
+    </Router>
+  );
 }
 
-export default App;
+export default App;
--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -2,9 +2,43 @@ import React from "react";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 class FriendsList extends React.Component {
+    state = {
+        friends: []
+    };
+    componentDidMount() {
+        this.getData();
+    }
+
+
+    getData = () => {
+        axiosWithAuth()
+        .get('/friends')
+        .then(res => {
+            console.log(res);
+            this.setState({...this.state, friends: res.data})
+        })
+        .catch(err => console.error(err));
+    }
+
     render(){
         return (
-            <div></div>
+            <div>
+                <h1>Friends</h1>
+                <div>
+
+                {this.state.friends.map(friends => (
+                    <div>
+                        <p>{friends.id}</p>
+                        <p>{friends.name}</p>
+                        <p>{friends.age}</p>
+                        <p>{friends.email}</p>
+                        
+                    </div>
+                ))}
+                </div>
+            </div>
         )
     }
 }
+
+export default FriendsList;
